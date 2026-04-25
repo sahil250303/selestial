f2c6d5b581c725aab50140e8e10da50e96f0d9be
Sahil <sahil@example.com>
2026-04-25 19:11:27 +0530

    Fix Vite chunk size warning for deployment

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,17 @@ export default defineConfig({
         changeOrigin: true
       }
     }
+  },
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   }
 })
